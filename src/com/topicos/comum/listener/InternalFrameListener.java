cd0a71eac3fcdcac3661b8769df5bda04f106e3d
gabrielgageiro <gabrielgageiro@gmail.com>
2018-04-13 15:39:24 -0300

    algumas coisas

---
+++ src/com/topicos/comum/listener/InternalFrameListener.java
@@ -0,0 +1,34 @@
+/*
+Criado por Gabriel Gageiro;
+Data:13/04/18
+*/
+
+
+package com.topicos.comum.listener;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public abstract class InternalFrameListener implements ActionListener {
+        private JButton btnCadastrar;
+     private JButton btnCancelar;
+
+
+        public InternalFrameListener(JButton btnCancelar,JButton btnCadastrar){
+            this.btnCancelar=btnCancelar;
+            this.btnCadastrar=btnCadastrar;
+        }
+        public void actionPerformed(ActionEvent evento) {
+            if(evento.getSource() == btnCancelar)
+                sobreItem();
+            else if(evento.getSource() == btnCadastrar)
+                btnCadastrar();
+
+        }
+
+        public abstract void btnCadastrar();
+
+        public abstract void sobreItem();
+
+}
---
+++ src/com/topicos/telas/internalFrame/PessoasLinstenner/PessoasInternalListener.java
@@ -0,0 +1,13 @@
+/*
+Criado por Gabriel Gageiro;
+Data:13/04/18
+*/
+
+
+package com.topicos.telas.internalFrame.PessoasLinstenner;
+
+import com.topicos.comum.listener.PrincipalListenner;
+
+public class PessoasInternalListener  {
+
+}

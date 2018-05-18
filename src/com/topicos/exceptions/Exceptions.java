eecd9dc484bc84d6256f54240000178004f9749d
marcelo <marcelombfilho@outlook.com>
2018-05-18 15:40:13 -0300

    Janela de Exceptions.

--- src/com/topicos/exceptions/Exceptions.java
+++ src/com/topicos/exceptions/Exceptions.java
@@ -1,11 +1,19 @@
 package com.topicos.exceptions;
 
+import javax.swing.*;
+
 public class Exceptions extends Exception{
     public Exceptions(){
         super("Ocorreu um erro!");
+        showWindow("Ocorreu um erro!");
     }
     public Exceptions(String msg){
         super(msg);
+        showWindow(msg);
+    }
+
+    public void showWindow(String msg){
+        JOptionPane.showMessageDialog(null,msg,"ERRO!",JOptionPane.ERROR_MESSAGE);
     }
 
 }

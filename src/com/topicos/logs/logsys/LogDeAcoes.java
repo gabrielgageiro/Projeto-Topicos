2affb04b273e51fb2982f259c9ebcb987741351c
gabrielgageiro <gabrielgageiro@gmail.com>
2018-06-03 20:05:04 -0300

    Adicionado datas

--- src/com/topicos/logs/logsys/LogDeAcoes.java
+++ src/com/topicos/logs/logsys/LogDeAcoes.java
@@ -6,48 +6,47 @@ Data:16/05/18
 
 package com.topicos.logs.logsys;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public final class  LogDeAcoes {
+public final class LogDeAcoes {
+
 
     private static String usuario;
     private static ArrayList<String> log = new ArrayList<>();
+    private static LocalDateTime localDateTime;
     private static final DateTimeFormatter FORMATADOR = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
-            .withLocale(new Locale("pt","br"));
+            .withLocale(new Locale("pt", "br"));
 
-    public LogDeAcoes(String usuario){
-        this.usuario=usuario;
+    public LogDeAcoes(String usuario) {
+        this.usuario = usuario;
 
     }
 
-
-    public static void salvarLog(String evento){
+    public static void salvarLog(String evento) {
         new Thread(() -> {
+            localDateTime = LocalDateTime.now();
+            String dados = localDateTime.format(FORMATADOR) + "\t" + "|" + "\t" + usuario +
+                    "\t" + "\t" + "|" + "\t" + evento + "\n";
+
             try {
-
-                LocalDateTime localDateTime = LocalDateTime.now();
-                String dados = localDateTime.format(FORMATADOR) + "\t" + "|" + "\t" + usuario +
-                        "\t" +   "\t" + "|"  + "\t" +  evento +"\n";
-
-                try {
-                    FileWriter fileWriter = new FileWriter("log.log",true);
-                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                    bufferedWriter.write(dados);
-                    bufferedWriter.flush();
-                    bufferedWriter.close();
+                FileWriter fileWriter = new FileWriter("log.log", true);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write(dados);
+                bufferedWriter.flush();
+                bufferedWriter.close();
 
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } catch (Exception e) {
+            } catch (IOException e) {
                 e.printStackTrace();
-                LogDeAcoes.salvarLog(e.getStackTrace().toString());
+                LogDeAcoes.salvarLog(e.getMessage());
+
             }
         }).start();
 
